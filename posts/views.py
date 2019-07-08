7cb542577673cdeb43b89cc2fc523440bb314a2d
Najmul Ahmed <najmul2022@gmail.com>
2019-07-08 18:15:04 +0600

    group join and leave implemented

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from django.http.response import  Http404
 # Create your views here.
 from django.views import generic
 from . import models
 from braces.views import SelectRelatedMixin
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import (LoginRequiredMixin,PermissionRequiredMixin)
 
 
 class PostList(SelectRelatedMixin, generic.ListView):
@@ -19,3 +21,34 @@ class PostDetail(SelectRelatedMixin, generic.DetailView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user__username_iexact=self.kwargs.get('username'))
+
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post_user= User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+        except User.DoesNotExist:
+            raise Http404
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user'] = self.post_user
+        return context
+
+
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    fields = ('message', 'group')
+    model = models.Post
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
